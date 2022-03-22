fb4affbb533994ad53e641781009203826bb0f56
nikita539 <evteev_n@inbox.ru>
2022-03-22 21:42:13 +0300

    fixed parcelrc file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import { getValue } from "./modules/get";
 const Handlebars = require("handlebars");
-import template from 'src/pages/log_in.hbs'
+import log from './pages/log_in.hbs'
 
-const helloWorld = {
-    hello: "hello",
-}
-
-const value = getValue(helloWorld, 'hello')
-
-console.log(typeof value)
+document.addEventListener('DOMContentLoaded', function () {
+    const templateFn = Handlebars.compile(log)
+    console.log(templateFn({ title: "Hello World!" }))
+})
